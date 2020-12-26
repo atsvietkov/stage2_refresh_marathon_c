904c94fd7c7ad76ca73e4a81218e0cebfe465374
Andii Tsvietkov <atsvietkov@student.ucode.world>
2020-12-26 14:25:40 +0200

    Task 10 fix

--- t10/mx_print_args.c
+++ t10/mx_print_args.c
@@ -1,10 +1,7 @@
-int mx_strlen(const char *s);
 void mx_printstr(const char *s);
 void mx_printchar(char c);
 
 int main(int argc, char **argv) {
-    // mx_printchar(argc + 48);
-    // mx_printchar(10);
     for (int i = 1; i < argc; i++) {
         mx_printstr(argv[i]);
         mx_printchar(10);
