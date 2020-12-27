d1c3dfbd2617e30fb71edadf068bf81ce03422a4
Андрій Цвєтков <atsvietkov@e1r4p8.unit.ua>
2020-12-27 15:57:25 +0200

    printint MIN_INT fix

--- t04/mx_printint.c
+++ t04/mx_printint.c
@@ -1,39 +1,50 @@
 #include <stdbool.h>
+#include <unistd.h>
 
 void mx_printchar(char c);
 
 void mx_printint(int n) {
-    bool negative = 0;
-    if (n < 0) {
-        n *= -1;
-        negative = 1;
-    }
-
-    int len = 0;
-    int n_cpy = n;
-    for ( ; n_cpy > 0; len++) {
-        n_cpy /= 10;
-    }
-
-    if (n == 0) {
-        mx_printchar(0+48);
+    if (n == -2147483648) {
+        write(1, "-2147483648", 11);
     }
     else {
-        int arr[len];
-        for(int i = 0; i < len; i++) {
-            arr[i] = n % 10;
-            n /= 10;
+        bool negative = 0;
+        if (n < 0) {
+            n *= -1;
+            negative = 1;
         }
-        if (negative == 1) {
-            mx_printchar('-');
+
+        int len = 0;
+        int n_cpy = n;
+        for ( ; n_cpy > 0; len++) {
+            n_cpy /= 10;
         }
-        for (int i = len-1; i >= 0; i--) {
-            mx_printchar(arr[i]+48);
+
+        if (n == 0) {
+            mx_printchar(0+48);
+        }
+        else {
+            int arr[len];
+            for(int i = 0; i < len; i++) {
+                arr[i] = n % 10;
+                n /= 10;
+            }
+            if (negative == 1) {
+                mx_printchar('-');
+            }
+            for (int i = len-1; i >= 0; i--) {
+                mx_printchar(arr[i]+48);
+            }
         }
     }
 }
 
-// int main() {
-//     mx_printint(-255);
-//     mx_printint(0);
-// }
+int main() {
+    mx_printint(-255);
+    mx_printchar(10);
+    mx_printint(0);
+    mx_printchar(10);
+    mx_printint(2147483647);
+    mx_printchar(10);
+    mx_printint(-2147483648);
+}
